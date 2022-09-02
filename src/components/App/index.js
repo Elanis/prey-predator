e0e623dd9b4b09c6e844728133dedbac7a2afc28
Elanis <elanis@hotmail.com>
2022-09-02 17:03:11 +0200

    Simple UI

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,48 @@
+import { Canvas2D, Circle, Polygon, Rect } from 'canvas2d-wrapper'
+
+import './index.css';
+
 export default function App() {
-	return null;
+	const elements = [];
+	for(let i = 0; i < 998; i++) {
+		let object = null;
+
+		object = new Circle({
+			id: i,
+			x: 50 - Math.round(Math.random() * 100),
+			y: 50 - Math.round(Math.random() * 100),
+			radius: 0.5,
+			fill: (Math.random() > 0.5) ? 'black' : undefined,
+			stroke: (Math.random() > 0.5) ? 'black' : undefined
+		});
+
+		if(!object.fill && !object.stroke) {
+			object.fill = 'green';
+		}
+
+		elements.push(object);
+	}
+
+	return (
+		<>
+			<Canvas2D 
+				elements={elements}
+				width={1200}
+				height={700}
+				minZoom={0.25}
+				maxZoom={4}
+				tileSize={32}
+				onClick={(e) => {
+					console.log('Click event:', e);
+				}}
+			/>
+
+			<input
+				type="button"
+				className="ui-button"
+				onClick="toggleTime"
+				value="Start/Pause"
+			/>
+		</>
+	);
 }
